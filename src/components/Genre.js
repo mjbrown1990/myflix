beb1a200e0ab1df386ac5c51fcec29281d799802
James Brown <mjbrown1990@hotmail.co.uk>
2020-05-17 16:35:37 +0100

    Removed cruft, still need to refactor this into a function component and use hooks instead.

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -9,17 +9,7 @@ import Slider from "./Slider";
 class Genre extends Component {
     constructor(props) {
         super(props);
-
         this.state = {};
-
-        // this.state = {
-        //     genreIDs: [
-        //         { id: 28, name: "Action" },
-        //         { id: 16, name: "Animation" },
-        //         { id: 35, name: "Comedy" },
-        //         { id: 12, name: "Adventure" }
-        //     ]
-        // };
     }
 
     componentDidMount() {
