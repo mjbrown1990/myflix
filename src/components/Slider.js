724092f113579df74259a334f1487a04ffd6a0c0
James Brown <mjbrown1990@hotmail.co.uk>
2020-05-17 16:36:12 +0100

    Improved the loading state of the slider component.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,38 +1,39 @@
 import React from 'react';
-import Skeleton from 'react-loading-skeleton';
-
-import LoadingSkeleton from "./LoadingSkeleton";
+import PropTypes from 'prop-types';
 
 const Slider = ({ movies }) => {
     if (movies.length === 0) {
         return (
-            <LoadingSkeleton>
-                <div className="movie-grid movie-grid--loading">
-                    <div className="loading__placeholder">
-                        <Skeleton height="100%" />
-                    </div>
-                    <div className="loading__placeholder">
-                        <Skeleton height="100%" />§
-                    </div>
-                    <div className="loading__placeholder">
-                        <Skeleton height="100%" />§
-                    </div>
-                    <div className="loading__placeholder">
-                        <Skeleton height="100%" />§
-                    </div>
-                </div>
-            </LoadingSkeleton >
+            <div className="movie-grid movie-grid--loading">
+                {[...Array(4)].map((_, i) => <div key={i} className="loading__placeholder" />)}
+            </div>
         )
     }
 
     return (
         <div className="movie-grid">
-            {movies.map(movie => (
-                <div key={movie.id} className="movie-grid__item">
-                    <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
-                </div>
-            ))}
+            {movies.map(movie => {
+                if (!movie.backdrop_path) {
+                    return null;//Not all movies returned by the API have a backdrop image!
+                }
+
+                return (
+                    <div key={movie.id} className="movie-grid__item">
+                        <img src={`https://image.tmdb.org/t/p/w300/${movie.backdrop_path}`} alt={movie.title} />
+                    </div>
+                )
+            })}
         </div>
+    )
+}
+
+Slider.propTypes = {
+    movies: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            backdrop_path: PropTypes.string
+        })
     )
 }
 
